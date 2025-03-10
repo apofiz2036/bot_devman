1c54bc616b2a4a8ee138e102381838e37de9e68b
Yuriy Mikhailov <apofiz2036@bk.ru>
2025-03-10 07:49:00 +0500

    исправил def main, заменил на logger.info("Тайм-аут запроса")

--- tg_bot.py
+++ tg_bot.py
@@ -20,6 +20,31 @@ class TelegramLogsHandler(logging.Handler):
 
 
 def main():
+    load_dotenv()
+    DEVMAN_TOKEN = os.environ['DEVMAN_TOKEN']
+    TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_TOKEN']
+    CHAT_ID = os.environ['CHAT_ID']
+    bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.WARNING)
+
+    rotating_handler = RotatingFileHandler(
+        'app.log',
+        maxBytes=20000,
+        backupCount=2
+    )
+    rotating_handler.setFormatter(logging.Formatter("%(process)d %(levelname)s %(message)s"))
+    logger.addHandler(rotating_handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logging.Formatter("%(process)d %(levelname)s %(message)s"))
+    logger.addHandler(stream_handler)
+
+    telegram_handler = TelegramLogsHandler(bot, CHAT_ID)
+    telegram_handler.setFormatter(logging.Formatter("%(process)d %(levelname)s %(message)s"))
+    logger.addHandler(telegram_handler)
+
     url = 'https://dvmn.org/api/long_polling/'
     headers = {
         "Authorization": f'Token {DEVMAN_TOKEN}'
@@ -56,7 +81,7 @@ def main():
                 text = f'Проверена работа "{lesson_title}", УРА! ПРИНЯТО!. .\n \n Ссылка на урок: {lesson_url}'
                 bot.send_message(chat_id=CHAT_ID, text=text)
         except requests.exceptions.ReadTimeout:
-            logger.debug("Тайм-аут запроса")
+            logger.info("Тайм-аут запроса")
         except requests.exceptions.Timeout:
             logger.error("Нет ответа от сервера")
         except requests.exceptions.ConnectionError:
@@ -68,29 +93,4 @@ def main():
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    DEVMAN_TOKEN = os.environ['DEVMAN_TOKEN']
-    TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_TOKEN']
-    CHAT_ID = os.environ['CHAT_ID']
-    bot = Bot(token=TELEGRAM_BOT_TOKEN)
-
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.WARNING)
-
-    rotating_handler = RotatingFileHandler(
-        'app.log',
-        maxBytes=20000,
-        backupCount=2
-    )
-    rotating_handler.setFormatter(logging.Formatter("%(process)d %(levelname)s %(message)s"))
-    logger.addHandler(rotating_handler)
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(logging.Formatter("%(process)d %(levelname)s %(message)s"))
-    logger.addHandler(stream_handler)
-
-    telegram_handler = TelegramLogsHandler(bot, CHAT_ID)
-    telegram_handler.setFormatter(logging.Formatter("%(process)d %(levelname)s %(message)s"))
-    logger.addHandler(telegram_handler)
-
     main()
